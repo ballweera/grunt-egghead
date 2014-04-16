ee7dbf56e132362e00791211e523839e48f983fc
Weera Kasetsin <wkasetsin@gmail.com>
2014-04-16 13:31:11 +0700

    Try multitask

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,13 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
-		conf: grunt.file.readJSON("config.json"),
-		uglify: {
-			dist: {
-				files: {
-					"dist/app.min.js": "app/**/*.js"
-				}
-			}
-		},
-		watch: {
-			files: "<%= conf.input %>",
-			tasks: ['uglify']
+		tasks: {
+			one: "first",
+			two: "second",
+			three: "third"
 		}
-	});
+	})
 
-	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.registerMultiTask("tasks", function() {
+		grunt.log.writeln(this.target + " " + this.data);
+	});
 }
